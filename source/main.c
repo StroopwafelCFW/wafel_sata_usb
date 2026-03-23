5d80702b47c164a2188bb6d1d2e6f2a4e38c8e07
Jan Hofmeier <jan.hofmeier@gmail.com>
2026-03-24 00:03:56 +0100

    use MLC key

--- source/main.c
+++ source/main.c
@@ -7,6 +7,31 @@
 #include <wafel/utils.h>
 #include <wafel/patch.h>
 #include <wafel/ios/svc.h>
+#include <wafel/trampoline.h>
+#include "wfs.h"
+
+
+// u32 sata_handle = 0;
+
+// static void hook_SHDD_server_hanlde(trampoline_state *regs){
+//     sata_handle = regs->r[0];
+//     debug_printf("SATAUSB: Setting SATA Server Handle to %d\n", sata_handle);
+// }
+
+static void wfs_initDeviceParams_exit_hook(trampoline_state *regs){
+    WFS_Device *wfs_device = (WFS_Device*)regs->r[5];
+    FSSALDevice *sal_device = FSSAL_LookupDevice(wfs_device->handle);
+    u32 server_handle = sal_device->server_handle;
+    debug_printf("SATAUSB: wfs_initDeviceParams_exit_hook server_handle: %p, SALDevice: %p\n", server_handle, sal_device);
+    if(server_handle == 0) { // Somehow this is 0 for SATA
+#if defined(NOCRYPTO)
+        wfs_device->crypto_key_handle = WFS_KEY_HANDLE_NOCRYPTO;
+#else
+        wfs_device->crypto_key_handle = WFS_KEY_HANDLE_MLC;
+#endif
+    }
+}
+
 
 // This fn runs before everything else in kernel mode.
 // It should be used to do extremely early patches
@@ -39,6 +64,9 @@ void kern_main()
 
     // usb to sdcard
     // ASM_PATCH_K(0x1077EDA0, "MOV R3, #6");
+
+    trampoline_hook_before(0x107435f4, wfs_initDeviceParams_exit_hook);
+    //trampoline_hook_before(0x10786b28, hook_SHDD_server_hanlde);
 }
 
 // This fn runs before MCP's main thread, and can be used
